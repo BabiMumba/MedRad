94a4f9fb57da188d9c40a987e7b7eeb6baaafa5d
Babi Mumba <babimumba243@gmail.com>
2023-07-26 16:20:59 +0200

    add new color theme

--- app/src/main/java/cd/babitech/medrad/Auth/LoginActivity.kt
+++ app/src/main/java/cd/babitech/medrad/Auth/LoginActivity.kt
@@ -3,10 +3,13 @@ package cd.babitech.medrad.Auth
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import cd.babitech.medrad.R
+import cd.babitech.medrad.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding= ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/cd/babitech/medrad/Auth/RegisterActivity.kt
+++ app/src/main/java/cd/babitech/medrad/Auth/RegisterActivity.kt
@@ -3,10 +3,15 @@ package cd.babitech.medrad.Auth
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import cd.babitech.medrad.R
+import cd.babitech.medrad.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding= ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
