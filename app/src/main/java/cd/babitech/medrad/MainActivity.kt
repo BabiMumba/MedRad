243e77f8e85454354776d6c0af1951a1ac6a5263
Babi Mumba <babimumba243@gmail.com>
2023-07-26 15:36:51 +0200

    first commit

---
+++ app/src/main/java/cd/babitech/medrad/MainActivity.kt
@@ -0,0 +1,11 @@
+package cd.babitech.medrad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
