3c8d1669b4f35806123d2b1dc7ba999d66bf2e95
Babi Mumba <babimumba243@gmail.com>
2023-07-31 16:36:01 +0200

    add new item docteur

--- app/src/main/java/cd/babitech/medrad/Fragment/HomeFragment.kt
+++ app/src/main/java/cd/babitech/medrad/Fragment/HomeFragment.kt
@@ -6,15 +6,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import cd.babitech.medrad.R
+import cd.babitech.medrad.databinding.FragmentHomeBinding
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.models.SlideModel
 
 class HomeFragment : Fragment() {
+    lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+
+
+        val imageList = ArrayList<SlideModel>() // Create image list
+        imageList.add(SlideModel("https://firebasestorage.googleapis.com/v0/b/restaurant-2506c.appspot.com/o/lushi_3.png?alt=media&token=bb41e27f-9a31-47b5-b871-99517f97dfa5", ScaleTypes.FIT))
+        imageList.add(
+            SlideModel("https://firebasestorage.googleapis.com/v0/b/restaurant-2506c.appspot.com/o/lstore.png?alt=media&token=25a865b3-304b-47b6-a2cc-1d986d5c5013",
+                ScaleTypes.FIT)
+        )
+
+        binding.imageSlider.setImageList(imageList)
+
+        return binding.root
     }
 
 }
