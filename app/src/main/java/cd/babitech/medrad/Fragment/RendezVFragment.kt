c0a13471973ca616682b94dd76c478869cab6a98
Babi Mumba <babimumba243@gmail.com>
2023-08-11 14:44:09 +0200

    new modification

--- app/src/main/java/cd/babitech/medrad/Fragment/RendezVFragment.kt
+++ app/src/main/java/cd/babitech/medrad/Fragment/RendezVFragment.kt
@@ -11,13 +11,13 @@ import cd.babitech.medrad.Adapter.RendezAdapter
 import cd.babitech.medrad.Model.rende_vous
 import cd.babitech.medrad.R
 import cd.babitech.medrad.Unit.DATA
+import cd.babitech.medrad.Unit.Void
 import cd.babitech.medrad.databinding.FragmentRendezVBinding
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
 class RendezVFragment : Fragment() {
-    private val contactInfo = arrayListOf<rende_vous>()
     private lateinit var fstore : FirebaseFirestore
     private lateinit var auth : FirebaseAuth
     private lateinit var rendeAdapter : RendezAdapter
@@ -50,27 +50,32 @@ class RendezVFragment : Fragment() {
     }
 
     fun getdata(){
+        val liste_rendev_v = arrayListOf<rende_vous>()
         binding.progressItem.loaderFrameLayout.visibility = View.VISIBLE
         fstore = FirebaseFirestore.getInstance()
         auth = FirebaseAuth.getInstance()
         fstore.collection(DATA.rendeVous)
-            .whereEqualTo("id_user",DATA.id_user)
             .get()
             .addOnSuccessListener {
             if (!it.isEmpty){
+                liste_rendev_v.clear()
                 val liste_rendev = it.documents
                 for(i in liste_rendev) {
                     val model = i.toObject(rende_vous::class.java)!!
-                    contactInfo.add(model)
-                    rendeAdapter = RendezAdapter(contactInfo)
+                    if (model.id_user==DATA.id_user){
+                        liste_rendev_v.add(model)
+                    }
+                    rendeAdapter = RendezAdapter(liste_rendev_v)
+                    rendeAdapter.notifyDataSetChanged()
 
                 }
                 binding.mayRendevz.adapter = rendeAdapter
 
-                if (contactInfo.isEmpty()){
+               // Void.toas(requireActivity(),"${liste_rendev_v.size}")
+                if (liste_rendev_v.size<=0){
                     Glide.with(this).asGif().load(R.raw.vide).into(binding.emptyListe)
                     binding.progressItem.loaderFrameLayout.visibility = View.GONE
-                    binding.emptyListe.visibility = View.VISIBLE
+                    binding.stateListe.visibility = View.VISIBLE
                 }else{
                     binding.progressItem.loaderFrameLayout.visibility = View.GONE
                 }
